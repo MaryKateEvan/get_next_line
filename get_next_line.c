cb017b27dc3aaf39e1ac9f4683ff27767386a7fd
Maria Aikaterini Evangelidou <mevangel@3-G-6.42heilbronn.de>
2023-06-10 21:29:10 +0200

    new additions

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: mevangel <mevangel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 14:18:34 by mevangel          #+#    #+#             */
-/*   Updated: 2023/06/07 18:08:34 by mevangel         ###   ########.fr       */
+/*   Updated: 2023/06/10 20:02:14 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-I must check that it works as expected both when reading a file and when reading from standard input. 
+I must check that it works as expected both when reading a file and when reading from standard input.
 
 the returned line should include the \n character (except for the end if it doesn't exist)
 
@@ -24,8 +24,25 @@ ssize_t read(int fd, void *buffer, size_t count);
 	it returns one of: the number of bytes read on success, or
 						0 if the end of the file or input source has been reached
 						-1 if an error occurs
+
+For the bonus: multiple file descriptors
 */
 
 #include <unistd.h> //for the read function
+#include "get_next_line.h"
 
 
+
+char	*get_next_line(int fd)
+{
+	
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+}
+
+/*
+what my get_next_line must return:
+- a string with the full line ending in a line break when there is one. Or
+- NULL, if an error occurs, or there's nothing more to read.
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mevangel <mevangel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/07 14:18:38 by mevangel          #+#    #+#             */
-/*   Updated: 2023/06/07 14:18:39 by mevangel         ###   ########.fr       */
+/*   Created: 2023/05/17 18:38:45 by mevangel          #+#    #+#             */
+/*   Updated: 2023/06/10 20:01:53 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+
+# define BUFFER_SIZE 42
+
+
+
+#endif
