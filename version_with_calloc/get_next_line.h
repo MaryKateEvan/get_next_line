0794e51f340000009415ec0d432760456e36a47a
Maria Aikaterini Evangelidou <mevangel@3-F-11.42heilbronn.de>
2023-06-19 03:10:34 +0200

    another version

---
+++ version_with_calloc/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mevangel <mevangel@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 18:38:45 by mevangel          #+#    #+#             */
+/*   Updated: 2023/06/19 01:46:53 by mevangel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, char c);
+char	*ft_calloc(size_t num);
+char	*ft_strjoin(char *stash, char *buffer);
+
+#endif
